25afbd086ec6a8238dbd19ab5cbcada9f2d928a1
mathlun <matheuslunadev@gmail.com>
2020-09-08 13:10:04 -0300

    removed application/**/*.json of the file .gitignore, because it’s not good for anything.

--- application/common/functions.js
+++ application/common/functions.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 
-function saveDataInFileJSON(jsonData) {           const jsonContent = JSON.stringify(jsonData);                                                                                 fs.writeFile('data.json',                         jsonContent,                              (err) => err ? console.log(err) : console.log('Pedido salvo com sucesso!'))                                   }
+function saveDataInFileJSON(jsonData) {
+	const jsonContent = JSON.stringify(jsonData);
+	fs.writeFile('data.json', jsonContent, (err) => err ? console.log(err) : console.log('Pedido salvo com sucesso'))
+
+}
 
 module.exports = {
 	saveDataInFileJSON
